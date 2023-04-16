f5660f36e4b0381ae0d0bc3b02f3857196262ca7
Dan Rice <dan.rice.92@outlook.com>
2023-04-16 13:53:30 -0600

    Revert "opacity and logo"
    This reverts commit 0965cfdfcc437349a9a58c0752cf8115523f9309.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import logo from './images/logo.svg';
-
 function App() {
   return (
-    <div>
-      <div className="absolute bg-[url('images/skyline.jpeg')] h-screen w-screen">
-        <div className="bg-white h-screen w-screen opacity-80"></div>
-      </div>
-      <div className="relative h-screen flex place-content-center leading-[100vh]">
-        <img src={logo} className="w-1/2" title="Bristle" />
-      </div>
-    </div>
+    <div className="flex h-screen w-screen bg-[url('images/skyline.jpeg')]"></div>
   );
 }
 
