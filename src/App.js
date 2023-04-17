b3e845bc83ccd735c3c958a71b28f62021eafe87
Dan Rice <dan.rice.92@outlook.com>
2023-04-17 08:07:51 -0600

    Start using hero icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './images/logo.svg';
+import HeroIcons from './icons/hero-icons';
 
 function App() {
   return (
@@ -6,8 +7,10 @@ function App() {
       <div className="absolute bg-[url('images/skyline.jpeg')] h-screen w-screen">
         <div className="bg-white h-screen w-screen opacity-80"></div>
       </div>
-      <div className="relative h-screen flex place-content-center leading-[100vh]">
-        <img src={logo} className="w-1/2" title="Bristle" />
+      <div className="relative h-screen flex flex-col place-content-center items-center">
+        <p>Launch your career with</p>
+        <img src={logo} className="w-1/2 pt-4 pb-4" title="Bristle" />
+        <HeroIcons.ChevronDoubleDown />
       </div>
     </div>
   );
