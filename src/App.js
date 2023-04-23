3281c764887eb0dd04665215d0059ea5b5d0208c
Dan Rice <dan.rice.92@outlook.com>
2023-04-23 15:55:50 -0600

    bootstrapified

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import logo from './images/logo.svg';
 function App() {
   return (
     <div id="app">
-      <div className="skyline-background">
-        <div className="container">
-          <img src={logo} className="img-fluid" title="Bristle" />
+      <div className="public-container">
+        <div className="row flex-column">
+          <div className="col d-flex justify-content-center">
+            <p>Launch your career with</p>
+          </div>
+          <div className="col d-flex justify-content-center">
+            <img src={logo} className="img-fluid" alt="Bristle logo" />
+          </div>
         </div>
       </div>
     </div>
