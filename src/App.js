2dc534c77a1234c49ebba389fd9d917fc728a161
Dan Rice <dan.rice.92@outlook.com>
2023-04-23 15:17:27 -0600

    use bootstrap and sass, remove tailwind for legible HTML

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
+import './App.scss';
 import logo from './images/logo.svg';
-import HeroIcons from './icons/hero-icons';
 
 function App() {
   return (
-    <div>
-      <div className="absolute bg-[url('images/skyline.jpeg')] h-screen w-screen">
-        <div className="bg-white h-screen w-screen opacity-80"></div>
-      </div>
-      <div className="relative h-screen flex flex-col place-content-center items-center">
-        <p>Launch your career with</p>
-        <img src={logo} className="w-1/2 pt-4 pb-4" title="Bristle" />
-        <HeroIcons.ChevronDoubleDown />
+    <div id="app">
+      <div className="skyline-background">
+        <div className="container">
+          <img src={logo} className="img-fluid" title="Bristle" />
+        </div>
       </div>
     </div>
   );
